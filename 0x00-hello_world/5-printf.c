90c83efab05d19d7ed94621f18e45e504e52a403
ThomasMugo <tmugo360@gmail.com>
2022-09-08 10:05:01 -0700

    Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - we will print our statement here
- * return: 0 if successful
+ * Return: 0 if successful
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
