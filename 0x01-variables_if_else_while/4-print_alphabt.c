48b8e95cf198f6cd9522d38d44a5490300211e7e
ThomasMugo <tmugo360@gmail.com>
2022-09-09 09:11:19 -0700

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - our functions go here
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x != 'e' && x != 'q')
+		{
+			putchar(x);
+		}
+		x++;
+	}
+	putchar (x);
+	return (0);
+}
