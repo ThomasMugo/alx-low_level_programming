fea6ce4c6c530b6baff04e3bdbca2315b45e62d0
ThomasMugo <tmugo360@gmail.com>
2022-09-09 07:24:06 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - our function comes here
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
