9e94895527646481a0bd137ad51071b338db7f11
ThomasMugo <tmugo360@gmail.com>
2022-09-12 15:06:01 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,10 @@ int main(void)
 	put_char('p');
 	put_char('u');
 	put_char('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	put_char('c');
+	put_char('h');
+	put_char('a');
+	put_char('r');
+	put_char('\n');
 	return (0);
 }
