414c730f088295eab754ebdf77a19a1d7006f7d8
ThomasMugo <tmugo360@gmail.com>
2022-09-13 09:27:30 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * jack_bauer - our statements are here
+ * @n: our character
+ * Return: 0 if successful
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; h < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
