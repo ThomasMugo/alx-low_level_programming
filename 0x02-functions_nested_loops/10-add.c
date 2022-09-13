a4c3855654ce1e7c4ba06c91a51572d75f5aef81
ThomasMugo <tmugo360@gmail.com>
2022-09-13 10:52:03 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - our statements are here
+ * @a: one character
+ * @b: another character
+ * Return: result
+ */
+int add(int a, int b,)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
