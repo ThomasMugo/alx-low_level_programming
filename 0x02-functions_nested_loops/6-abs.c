ccc5015b16138103cfa85d4367179e5e06ab23c9
ThomasMugo <tmugo360@gmail.com>
2022-09-13 04:47:47 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - our statements are here
+ * @n: our character
+ * Return: 0 if successful
+ */
+int _abs(int)
+{
+	int n;
+	
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar(n);
+	}
+	return (0);
+}
