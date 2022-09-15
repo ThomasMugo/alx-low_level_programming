8dd053331722245c1600ce9ada1dbbaad19da54b
ThomasMugo <tmugo360@gmail.com>
2022-09-15 13:41:29 -0700

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - our statements are here
+ * @size: our characters
+ * Return: void
+ */
+void print_square(void)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
