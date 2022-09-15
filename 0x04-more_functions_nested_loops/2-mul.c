75bb7541d58740a0d7e568a730d827f4bf786966
ThomasMugo <tmugo360@gmail.com>
2022-09-15 01:51:00 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * mul - our statements are here
+ * @a: our character
+ * @b: our character
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
