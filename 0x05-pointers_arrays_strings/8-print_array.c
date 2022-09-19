1790c924e0340d07a8e35db3e6c5a1c284d95df1
ThomasMugo <tmugo360@gmail.com>
2022-09-19 13:03:02 -0700

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - our statements are here
+ * @a: our pointer
+ * @n: our character
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
