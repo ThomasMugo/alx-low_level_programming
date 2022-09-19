b94ab23d67a851e01795a1d7566193be1694e96e
ThomasMugo <tmugo360@gmail.com>
2022-09-19 12:08:38 -0700

    Print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + 1))
+	while (*(s + i))
 	{
 		i++;
 	}
 	i = i - 1;
 	while (i >= 0)
 	{
-		_putchar(*(i + i));
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');
