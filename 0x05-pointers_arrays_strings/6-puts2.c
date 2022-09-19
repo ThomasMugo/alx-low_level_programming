fadac435aaf62c1920d2588dcc555f13b590ae6e
Thomas Mugo <47977441+ThomasMugo@users.noreply.github.com>
2022-09-19 22:47:35 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len = 0;
 
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
