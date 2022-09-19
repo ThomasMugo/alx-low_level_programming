3843701f51a2326406ae9cab68913b7bbb49543f
ThomasMugo <tmugo360@gmail.com>
2022-09-19 05:21:31 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - our statements are here
+ * *s: our pointer
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	s = _strlen;
+
+	while (*s != 0)
+	{
+		i++;
+		*s++;
+	}
+	return (i);
+}
