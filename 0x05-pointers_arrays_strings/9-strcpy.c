8833ee9c1e1511e3f626b9bdf7fc062c2b04f740
Thomas Mugo <47977441+ThomasMugo@users.noreply.github.com>
2022-09-19 23:23:06 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,9 +15,6 @@ char *_strcpy(char *dest, char *src)
 		*(dest + i) = *(src + i);
 		i++;
 	}
-<<<<<<< HEAD
-=======
 	*(dest + i) = '\0';
->>>>>>> e749e622eec6fa0d05eef5c71965272e03baff11
 	return (dest);
 }
