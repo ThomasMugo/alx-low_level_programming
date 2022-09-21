150ae53b9b36fb4cb52010be11d3280a205eab15
ThomasMugo <tmugo360@gmail.com>
2022-09-21 06:36:57 -0700

    String toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - change letters to uppercase
+ * @s: our pointer
+ * Return: s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while(*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) -= 'a' - 'A';
+		}
+		i++;
+	}
+	return (s);
+}
