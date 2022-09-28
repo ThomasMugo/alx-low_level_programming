a6399d98f75996b15f55a29534430f6f51974a7a
Thomas Mugo <47977441+ThomasMugo@users.noreply.github.com>
2022-09-28 17:41:47 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,28 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - finds the natural square root
- * @n: our number
- * Return: the natural square root
+ * sqrt_check - checks for the square root of b
+ * @a: our guess at sqrt
+ * @b: number to find sqrt 
+ * Return: value of the square root
+ */
+int sqrt_check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (sqrt_check(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: our integer
+ * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
+}
