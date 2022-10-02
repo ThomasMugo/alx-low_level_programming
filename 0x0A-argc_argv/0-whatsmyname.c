8a45cdac251415df0b5c7056012beae2074769b4
ThomasMugo <tmugo360@gmail.com>
2022-10-02 05:35:14 -0700

    Whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: argument array
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
