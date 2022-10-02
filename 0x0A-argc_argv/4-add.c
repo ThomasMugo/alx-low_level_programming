a73157af9655fc6a1ddcd9b5c0ff451dc120869a
Thomas Mugo <47977441+ThomasMugo@users.noreply.github.com>
2022-10-02 16:32:55 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,3 +7,24 @@
  * Return: 0 if successful
  */
 int main(int argc, int *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
