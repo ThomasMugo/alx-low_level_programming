a80ebb3f2aa99240f51b49683f74fbaf69678d4c
ThomasMugo <tmugo360@gmail.com>
2022-10-06 11:34:47 -0700

    String nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,10 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s = malloc(sizeof(char) * (a + b + 1));
 	if (s == NULL)
 		return (NULL);
-	for (c = 0; c < i; c++)
+	for (c = 0; c < a; c++)
 		s[c] = s1[c];
-	for (c = 0; c < j; c++)
-		s[c + i] = s2[c];
+	for (c = 0; c < b; c++)
+		s[c + a] = s2[c];
 	s[a + b] = '\0';
 	return (s);
 }
